b8208852cec36c65f7eb7063c50406f1cb0fcafa
Mohammad Reza Rajaee <m.r.r.sayron@gmail.com>
2026-06-07 09:55:30 +0430

    mlflow added

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import sys
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
 from networksecurity.exceptions.exception import NetworkSecurityException
 from networksecurity import logger
-from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
 if __name__ == '__main__':
@@ -36,5 +37,16 @@ if __name__ == '__main__':
         print(data_transformation_artifact)
         logger.info("data Transformation completed")
 
+          
+        logger.info("Model Training started")
+        model_trainer_config = ModelTrainerConfig(trainingpipelineconfig)    
+        model_trainer = ModelTrainer(
+                model_trainer_config=model_trainer_config,
+                data_transformation_artifact=data_transformation_artifact
+            )    
+        model_trainer_artifact = model_trainer.initiate_model_trainer()
+            
+        logger.info(f"Model Training artifact created: {model_trainer_artifact}")
+        
     except Exception as e:
         raise NetworkSecurityException(e, sys)
